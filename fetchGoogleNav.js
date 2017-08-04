77966c93546c6ca5ede047b0ff664c796622ec38
Sourav De <srvz39@gmail.com>
2017-08-04 14:26:49 +0530

    nav scarping from google finance working

--- fetchGoogleNav.js
+++ fetchGoogleNav.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var rp = require('request-promise');
 const cheerio = require('cheerio');
 var fs = require('fs');
 var _ = require('lodash');
@@ -33,12 +34,46 @@ for(i=0;i<fundList.length;i++){
     fundSym[i] = fundList[i] .toString().trim().split(',')[1];
 }
 
-console.log(fundList[1]);
-console.log(fundName[1]);
-console.log(fundSym[1]);
+userAgents = readtoArray('UserAgents.txt');
+//console.log(fundList[1]);
+//console.log(fundName[1]);
+//console.log(fundSym[1]);
 
+proxy = 'http://proxy.intra.bt.com:8080';
 url = 'https://www.google.com/finance/historical?q=MUTF_IN%3AHDFC_LIQU_DIR_1SY277O&startdate=Aug+1%2C+2001&enddate=Aug+2%2C+2017&noIL=1&num=30000&ei=O6iBWZitGI67ugS8vYeQBw';
-fundName = '';
-startDate = '';
-endDate = '';
-url = 'https://www.google.com/finance/historical?q='+fundName+'&startdate='+startDate+'&enddate='+endDate+'&noIL=1&num=30000';
+//https://www.google.com/finance/historical?q=MUTF_IN:HDFC_LIQU_DIR_1SY277O&startdate=Aug+1,+2001&enddate=Aug+2,+2017&noIL=1&num=30000&ei=O6iBWZitGI67ugS8vYeQBw
+fund = fundSym[getRandomInt(1,200)];
+startDate = 'Aug 1, 2014';
+endDate = 'Aug 2, 2017';
+url = 'https://www.google.com/finance/historical?q=MUTF_IN%3A'+fund+'&startdate='+startDate+'&enddate='+endDate+'&noIL=1&num=30000&ei=O6iBWZitGI67ugS8vYeQBw';
+//url = encodeURI(url);
+url1 = 'https://www.google.com/finance/historical?q=MUTF_IN:'+fund+'&startdate='+startDate+'&enddate='+endDate+'&noIL=1&num=30000';
+console.log(url);
+console.log(encodeURI(url1));
+
+var options = {  
+  uri: url,
+  proxy: proxy, //'http://' + proxyList[getRandomInt(0,25)], //proxy,
+  headers: {'User-Agent': userAgents[getRandomInt(0,25)].replace(/"/g,'').trim()},
+  transform: function (body, response, resolveWithFullResponse) {
+        return body;
+    }
+}
+
+rp(options)
+    .then(function(body){
+        $ = cheerio.load(body);
+        navtable = $('#prices');
+        //gf-table historical_price
+        //console.log(body);
+        console.log(navtable.text());
+        fs.writeFileSync('googNavop.txt',navtable.text(),function(err){
+            console.log('file saved!');
+        });
+    })
+    .catch(function(error){
+        //console.log(error);
+        fs.appendFile('errorlog.txt',error.toString(),function(err){
+            console.log('Error!');
+        });
+    })
