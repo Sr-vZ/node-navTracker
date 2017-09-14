259e00c682ce216e8013d7212570b34d4b68e530
Sourav De <srvz39@gmail.com>
2017-09-14 21:32:01 +0530

    listFunds updated

--- routes/listFunds.js
+++ routes/listFunds.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  fs.readFile( __dirname + "data/" + "googMFsym.csv", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
+    
+    res.end( "test");
+    fs.readFile();
 })
 
 
